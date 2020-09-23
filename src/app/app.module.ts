6979a5dd7fa752142d64fbbf39ebc6b8b4d2972f
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-09-23 21:17:36 +0300

    Add initial services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderModule } from './components/header/header.module';
+import { ServicesModule } from './services/services.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule, ServicesModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
