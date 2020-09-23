8f1fae23aeb7140391b26826d9a1f3767e25b144
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-09-23 21:22:51 +0300

    Added group-mover route

--- src/app/services/services-routing.module.ts
+++ src/app/services/services-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { GroupMoverComponent } from './group-mover/group-mover.component';
+import { GroupMoverModule } from './group-mover/group-mover.module';
 
-const routes: Routes = [{ path: '/services', children: [] }];
+const routes: Routes = [{ path: '/services', children: [{ path: '/group-mover', component: GroupMoverComponent }] }];
 
 @NgModule({
-	imports: [CommonModule, RouterModule.forChild(routes)],
+	imports: [CommonModule, RouterModule.forChild(routes), GroupMoverModule],
 	exports: [RouterModule],
 })
 export class ServicesRoutingModule {}
