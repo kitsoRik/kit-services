717ea19ce10653c62e91ab306b740a60eaa07114
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-09-28 21:00:05 +0300

    Added first service route

--- src/app/services/services-routing.module.ts
+++ src/app/services/services-routing.module.ts
@@ -4,10 +4,10 @@ import { CommonModule } from '@angular/common';
 import { GroupMoverComponent } from './group-mover/group-mover.component';
 import { GroupMoverModule } from './group-mover/group-mover.module';
 
-const routes: Routes = [{ path: '/services', children: [{ path: '/group-mover', component: GroupMoverComponent }] }];
+const routes: Routes = [{ path: 'services', children: [{ path: 'group-mover', component: GroupMoverComponent }] }];
 
 @NgModule({
 	imports: [CommonModule, RouterModule.forChild(routes), GroupMoverModule],
 	exports: [RouterModule],
 })
-export class ServicesRoutingModule {}
+export class ServicesRoutingModule { }
