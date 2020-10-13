c79c70e24eff0993498c0110fe4132b45c4815dc
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-13 21:10:59 +0300

    added github link to header

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { MaterialModule } from 'src/app/material/material.module';
+import { IconsModule } from 'src/app/icons/icons.module';
 
 @NgModule({
 	declarations: [HeaderComponent],
 	exports: [HeaderComponent],
-	imports: [CommonModule, MaterialModule],
+	imports: [CommonModule, MaterialModule, IconsModule],
 })
 export class HeaderModule {}
