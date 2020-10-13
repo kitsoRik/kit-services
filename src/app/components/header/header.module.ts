05fd579209260a5c9d500f547c6f4ac13ade505c
Rostyslav Pidburachynskyi <gamedevrostik@gmail.com>
2020-10-13 21:26:16 +0300

    added buttom to go to home page

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { MaterialModule } from 'src/app/material/material.module';
 import { IconsModule } from 'src/app/icons/icons.module';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
 	declarations: [HeaderComponent],
 	exports: [HeaderComponent],
-	imports: [CommonModule, MaterialModule, IconsModule],
+	imports: [CommonModule, AppRoutingModule, MaterialModule, IconsModule],
 })
 export class HeaderModule {}
